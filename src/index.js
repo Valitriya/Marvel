adc3b26c9888d4eb26bcc717e5bf6afd782c5440
Valli <nekotyantv@gmail.com>
2022-10-04 01:32:28 +0300

    add ErrorMessage and Spiner

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from './components/app/App';
 import './style/style.scss';
 
 
-
-;
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,3 +1,4 @@
+//Класс на чистом JS, сетевая часть
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=c77128a809855b0a1f17233c400eb41c";
@@ -11,29 +12,31 @@ class MarvelService {
 
     return await res.json();
   };
-
-  getAllCharacters = async() => {
+//Получение всех персонажей
+  getAllCharacters = async () => {
     const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=300&${this._apiKey}`
     );
     return res.data.results.map(this._transformCharacter);
   };
-
-  getCharacter = async(id) => {
-    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+//Получение одного персонажа
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
     return this._transformCharacter(res.data.results[0]);
-  }
+  };
 
-//получение и трансформирование данных 
+//Получение и трансформирование данных
   _transformCharacter = (char) => {
     return {
-        name: char.name,
-        description: char.description,
-        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
-        homepage: char.urls[0].url,
-        wiki: char.urls[1].url
-    }
-  }
+      name: char.name,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  };
 }
 
 export default MarvelService;
