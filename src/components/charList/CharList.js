49f0670c69606b110c500c322bee915ce04427c1
Valli <nekotyantv@gmail.com>
2022-10-13 23:05:23 +0300

    use ref to CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,118 +1,146 @@
-import { Component } from 'react/cjs/react.production.min';
-import PropTypes from 'prop-types';
+import { Component } from "react/cjs/react.production.min";
+import PropTypes from "prop-types";
 
-import MarvelService from '../../services/MarvelService';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import Spinner from '../spinner/Spinner';
+import MarvelService from "../../services/MarvelService";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import Spinner from "../spinner/Spinner";
 
-import './charList.scss';
+import "./charList.scss";
 
-class CharList extends Component { 
+class CharList extends Component {
+  state = {
+    charList: [],
+    loading: true,
+    error: false,
+    newItemLoading: false,
+    offset: 210,
+    charEnded: false,
+  };
 
-    state = {
-        charList: [],
-        loading: true,
-        error: false,
-        newItemLoading: false,
-        offset: 210,
-        charEnded: false
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.onRequest();
+  }
+  // Метод, отвечающий за прогрузку дополнительных 9 персонажей
+  onRequest = (offset) => {
+    this.onCharListLoading();
+    this.marvelService
+      .getAllCharacters(offset)
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  };
+  // Метод, отвечающий за запуск запроса
+  onCharListLoading = () => {
+    this.setState({
+      newItemLoading: true,
+    });
+  };
+
+  // Метод, отвечающий за успешную загрузку
+  onCharListLoaded = (newCharList) => {
+    // Удаление кнопки из видимости, когда все персонажи прогружены
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
     }
+    this.setState(({ offset, charList }) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended,
+    }));
+  };
 
-    marvelService = new MarvelService();
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
+  };
 
-    componentDidMount() {
-       this.onRequest();
-    }
-    // Метод, отвечающий за прогрузку дополнительных 9 персонажей
-    onRequest = (offset) => {
-        this.onCharListLoading();
-        this.marvelService.getAllCharacters(offset)
-        .then(this.onCharListLoaded)
-        .catch(this.onError)
-    }
-    // Метод, отвечающий за запуск запроса
-    onCharListLoading = () => {
-        this.setState({
-            newItemLoading: true
-        })
-    }
+  itemRef = [];
 
-    // Метод, отвечающий за успешную загрузку
-    onCharListLoaded = (newCharList) => {
-    // Удаление кнопки из видимости, когда все персонажи прогружены 
-        let ended = false;
-        if(newCharList .length < 9){
-            ended = true;
-        }
-        this.setState(({offset, charList}) => ({
-            charList: [...charList, ...newCharList],
-            loading: false,
-            newItemLoading: false,
-            offset: offset + 9,
-            charEnded: ended
-        }))
-    }
+  setRef = (ref) => {
+    this.itemRef.push(ref);
+  };
+  // Реализация и с классом и фокусом
+  focusOnItem = (id) => {
+    this.itemRef.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    this.itemRef[id].classList.add("char__item_selected");
+    this.itemRef[id].focus();
+  };
 
-    onError = () => {
-        this.setState({
-            error: true,
-            loading: false
-        })
-    }
+  // Метод для оптимизации вне конструкции метода render
+  renderItems(arr) {
+    const items = arr.map((item, i) => {
+      let imgStyle = { objectFit: "cover" };
+      if (
+        item.thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+      ) {
+        imgStyle = { objectFit: "unset" };
+      }
 
-    // Метод для оптимизации вне конструкции метода render
-    renderItems(arr) {
-        const items = arr.map((item) => {
-            let imgStyle = {'objectFit' : 'cover'};
-            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-                imgStyle = {'objectFit' : 'unset'};
+      return (
+        <li
+          className="char__item"
+          tabIndex={0}
+          ref={this.setRef}
+          key={item.id}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyPress={(e) => {
+            if (e.key === "" || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
             }
-
-            return(
-                <li className="char__item"
-                key = {item.id}
-                onClick={() => this.props.onCharSelected(item.id)}>
-                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-                    <div className="char__name">{item.name}</div>
-                </li>
-            )
-        });
+          }}
+        >
+          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+          <div className="char__name">{item.name}</div>
+        </li>
+      );
+    });
     // Конструкция вынесена для центровки спиннера/ошибки
-        return(
-            <ul className="char__grid">
-                {items}
-            </ul>
-        )
-    }
+    return <ul className="char__grid">{items}</ul>;
+  }
 
-    render() {
+  render() {
+    const { charList, loading, error, newItemLoading, offset, charEnded } =
+      this.state;
 
-        const {charList, loading, error, newItemLoading, offset, charEnded} = this.state;
+    const items = this.renderItems(charList);
 
-        const items = this.renderItems(charList);
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
 
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error) ? items : null;
-
-        return (
-            <div className="char__list">
-                {errorMessage}
-                {spinner}
-                {content}
-                <button 
-                className="button button__main button__long"
-                disabled={newItemLoading}
-                style={{'display': charEnded ? 'none' : 'block'}}
-                onClick={() => this.onRequest(offset)}>
-                    <div className="inner">load more</div>
-                </button>
-            </div>)    
-    }   
+    return (
+      <div className="char__list">
+        {errorMessage}
+        {spinner}
+        {content}
+        <button
+          className="button button__main button__long"
+          disabled={newItemLoading}
+          style={{ display: charEnded ? "none" : "block" }}
+          onClick={() => this.onRequest(offset)}
+        >
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
 }
 
 CharList.propTypes = {
-    onCharSelected: PropTypes.func.isRequired
-}
+  onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
